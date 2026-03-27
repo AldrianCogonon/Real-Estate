8be872135d62aba258185f016e37684a0f4c6080
Drake Rey Del <maccogoth@gmail.com>
2026-03-27 20:49:57 +0800

    updated

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c343e3aa5defed16abb8785fe3422a7e654b99e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+916a848f8e2cc64b015c71181255250235fc55b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
